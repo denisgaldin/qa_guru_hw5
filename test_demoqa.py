9d5261becde0f6f63fe7157a9814c344c3b141c7
Denis <denisgaldin@mail.ru>
2024-10-01 00:34:59 +0300

    Update 4 line - def test_demo_qa

--- test_demoqa.py
+++ test_demoqa.py
@@ -3,8 +3,8 @@ from selene import browser, have, be
 
 def test_demo_qa():
     browser.open("")
+    browser.element('#firstName').type('Denis')
     browser.element('#lastName').type('Galdin')
     browser.element('#userEmail').type('testqa_mail@yahoo.com')
     browser.all('#genterWrapper').element_by(have.attribute('value' 'Male')).element('/label').click()
     browser.all('#userNumber-label').type('+79009009000')
-    browser.element('#firstName').type('Denis')
