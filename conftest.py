aa58dcc0b38bbacc1df6812cea591a0adef56058
denisgaldin <denisgaldin@mail.ru>
2024-10-01 17:37:24 +0300

    Update code

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from selene import browser
 from selenium import webdriver
 
 
-@pytest.fixture
+@pytest.fixture(scope='function', autouse=True)
 def set_browser_size():
     browser.driver.set_window_size(1920, 1080)
 
@@ -12,8 +12,11 @@ def set_browser_size():
 def browser_management():
     browser.config.base_url = 'https://demoqa.com/'
     driver_options = webdriver.ChromeOptions()
-    #driver_options.add_argument('--headless=new')
+    # driver_options.add_argument('--headless=new')
+    driver_options.page_load_strategy = 'eager'
+
     browser.config.driver_options = driver_options
+    browser.open('https://demoqa.com/automation-practice-form')
 
     yield
 
--- test_demoqa.py
+++ test_demoqa.py
@@ -1,30 +1,52 @@
-from selene import browser, have
-import os
+from os import path
+from selene import be, browser, command, have
 
 
-def test_demo_qa():
-    browser.open('automation-practice-form')
+def test_fill_in_practice_form(browser_management):
+    browser.open('https://demoqa.com/automation-practice-form')
     browser.driver.execute_script("$('#fixedban').remove()")
     browser.driver.execute_script("$('footer').remove()")
 
     browser.element('#firstName').type('Denis')
     browser.element('#lastName').type('Galdin')
-    browser.element('#userEmail').type('testqa_mail@yahoo.com')
-    browser.all('#genterWrapper').element_by(have.attribute('value' 'Male')).element('/label').click()
-    browser.element('#userNumber').type('+79009009000')
-    browser.element("#dateOfBirthInput").click()
-    browser.element("//div[contains(@class, 'react-datepicker__month')]//div[contains(text(), '27')]").click()
-    browser.element("//div[contains(@class, 'react-datepicker__year')]//div[contains(text(), '1997')]").click()
-    browser.element("#subjectsInput").type('Endi')
-    browser.element("//label[text()='Sports']").click()
-    browser.element('#uploadFile').upload_file(os.path.abspath('C:\\Desktop\\картинки\\pic.jpg'))
+    browser.element('#userEmail').type('testqa_qa@mail.ru')
+    browser.all('[name="gender"]').element_by(have.attribute('value', 'Male')).element('../label').click()
+    browser.element('#userNumber').type('9009009000')
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.element('option[value="8"]').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('option[value="1997"]').click()
+    browser.element('.react-datepicker__day--027').click()
+    browser.element('#subjectsInput').type('e')
+    browser.element('.subjects-auto-complete__menu-list').element('//*[text()="English"]').click()
+    browser.element('#subjectsInput').type('a')
+    browser.element('.subjects-auto-complete__menu-list').element('//*[text()="Arts"]').perform(
+        command.js.scroll_into_view).click()
+    browser.element('//label[text()="Sports"]').click()
+    browser.element('#uploadPicture').set_value(
+        path.abspath(path.join('/Users/denisgaldin/Desktop/image.jpg'))
+    )
     browser.element('#currentAddress').type('9153 Jerry Dr, Juneau, Alaska 99801, USA')
     browser.element('#state').click()
-    browser.all('#state div[id^="react-select-"]').element('Haryana').click()
+    browser.element('//*[text()="Uttar Pradesh"]').click()
     browser.element('#city').click()
-    browser.all('#city div[id^="react-select-"]').element('Panipat').click()
-    browser.element('#submit').click()
+    browser.element('//*[text()="Lucknow"]').click()
+    browser.element('#dateOfBirthInput').should(have.attribute('value', '27 Sep 1997'))
+    browser.all('.subjects-auto-complete__multi-value__label').should(have.exact_texts('English', 'Arts'))
+    browser.element('.subjects-auto-complete__multi-value__remove').should(be.visible)
+    browser.all('[type="checkbox"]').first.should(have.attribute('value', '1'))
+    browser.element('button#submit').click()
 
-    browser.all('//tr//td[2]').should(have.exact_texts('Denis Galdin', 'testqa_mail@yahoo.com', 'Male', '79009009000',
-                                                       '27 January 1997', 'Endi', 'Sports', 'pic.jpg',
-                                                       '9153 Jerry Dr, Juneau, Alaska 99801, USA', 'Haryana Panipat'))
+    browser.element('table').should(be.visible)
+    browser.element('//table//td[text()="Student Name"]/../td[2]').should(have.exact_text('Denis Galdin'))
+    browser.element('//table//td[text()="Student Email"]/../td[2]').should(have.exact_text('testqa_qa@mail.ru'))
+    browser.element('//table//td[text()="Gender"]/../td[2]').should(have.exact_text('Male'))
+    browser.element('//table//td[text()="Mobile"]/../td[2]').should(have.exact_text('9009009000'))
+    browser.element('//table//td[text()="Date of Birth"]/../td[2]').should(have.exact_text('27 September, 1997'))
+    browser.element('//table//td[text()="Subjects"]/../td[2]').should(have.exact_text('English, Arts'))
+    browser.element('//table//td[text()="Hobbies"]/../td[2]').should(have.exact_text('Sports'))
+    browser.element('//table//td[text()="Picture"]/../td[2]').should(have.exact_text('image.jpg'))
+    browser.element('//table//td[text()="Address"]/../td[2]').should(
+        have.exact_text('9153 Jerry Dr, Juneau, Alaska 99801, USA'))
+    browser.element('//table//td[text()="State and City"]/../td[2]').should(have.exact_text('Uttar Pradesh Lucknow'))
